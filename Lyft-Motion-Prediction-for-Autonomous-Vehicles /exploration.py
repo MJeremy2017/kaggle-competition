fdf2508fdae1786b2ebacf6859e806c4c2c51954
jeremy.zhang <zhangyue9306@gmail.com>
2020-09-29 11:29:15 +0800

    add vis

---
+++ Lyft-Motion-Prediction-for-Autonomous-Vehicles /exploration.py
@@ -0,0 +1,96 @@
+# https://www.kaggle.com/corochann/lyft-comprehensive-guide-to-start-competition#Environment-setup
+import gc
+import os
+from pathlib import Path
+import random
+import sys
+
+from tqdm.notebook import tqdm
+import numpy as np
+import pandas as pd
+import scipy as sp
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from IPython.core.display import display, HTML
+
+# --- plotly ---
+from plotly import tools, subplots
+import plotly.offline as py
+
+py.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import plotly.express as px
+import plotly.figure_factory as ff
+import plotly.io as pio
+
+pio.templates.default = "plotly_dark"
+
+# --- models ---
+from sklearn import preprocessing
+from sklearn.model_selection import KFold
+import lightgbm as lgb
+import xgboost as xgb
+import catboost as cb
+
+pd.set_option('max_columns', 50)
+
+from l5kit.data import ChunkedDataset, LocalDataManager
+from l5kit.dataset import EgoDataset, AgentDataset
+
+from l5kit.rasterization import build_rasterizer
+from l5kit.configs import load_config_data
+from l5kit.visualization import draw_trajectory, TARGET_POINTS_COLOR
+from l5kit.geometry import transform_points
+from tqdm import tqdm
+from collections import Counter
+from l5kit.data import PERCEPTION_LABELS
+from prettytable import PrettyTable
+
+import os
+
+from matplotlib import animation, rc
+from IPython.display import HTML
+
+# set env variable for data
+os.environ["L5KIT_DATA_FOLDER"] = "/kaggle/input/lyft-motion-prediction-autonomous-vehicles"
+# get config
+cfg = load_config_data("/kaggle/input/lyft-config-files/visualisation_config.yaml")
+
+# loading data
+
+dm = LocalDataManager()
+dataset_path = dm.require('scenes/sample.zarr')
+zarr_dataset = ChunkedDataset(dataset_path)
+zarr_dataset.open()
+print(zarr_dataset)
+
+frames = zarr_dataset.frames
+
+# This is much faster!
+coords = frames["ego_translation"][:, :2]
+
+plt.scatter(coords[:, 0], coords[:, 1], marker='.')
+axes = plt.gca()
+axes.set_xlim([-2500, 1600])
+axes.set_ylim([-2500, 1600])
+plt.title("ego_translation of frames")
+
+semantic_rasterizer = build_rasterizer(cfg, dm)
+semantic_dataset = EgoDataset(cfg, zarr_dataset, semantic_rasterizer)
+
+
+def visualize_trajectory(dataset, index, title="target_positions movement with draw_trajectory"):
+    data = dataset[index]
+    im = data["image"].transpose(1, 2, 0)
+    im = dataset.rasterizer.to_rgb(im)
+    target_positions_pixels = transform_points(data["target_positions"] + data["centroid"][:2], data["world_to_image"])
+    draw_trajectory(im, target_positions_pixels, data["target_yaws"], TARGET_POINTS_COLOR)
+
+    plt.title(title)
+    plt.imshow(im[::-1])
+    plt.show()
+
+
+visualize_trajectory(semantic_dataset, index=3)
