74da58c0837901822a9946ba067c551be2b8f3db
jeremy.zhang <zhangyue9306@gmail.com>
2020-10-09 15:58:15 +0800

    add multi-modal

---
+++ Lyft-Motion-Prediction-for-Autonomous-Vehicles /res-multi-modal.py
@@ -0,0 +1,262 @@
+import gc
+import os
+from pathlib import Path
+import random
+import sys
+
+from tqdm.notebook import tqdm
+import numpy as np
+import pandas as pd
+import scipy as sp
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from IPython.core.display import display, HTML
+
+# --- plotly ---
+from plotly import tools, subplots
+import plotly.offline as py
+py.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import plotly.express as px
+import plotly.figure_factory as ff
+import plotly.io as pio
+pio.templates.default = "plotly_dark"
+
+# --- models ---
+from sklearn import preprocessing
+from sklearn.model_selection import KFold
+import lightgbm as lgb
+import xgboost as xgb
+import catboost as cb
+
+# --- setup ---
+pd.set_option('max_columns', 50)
+
+import zarr
+
+import l5kit
+from l5kit.data import ChunkedDataset, LocalDataManager
+from l5kit.dataset import EgoDataset, AgentDataset
+
+from l5kit.rasterization import build_rasterizer
+from l5kit.configs import load_config_data
+from l5kit.visualization import draw_trajectory, TARGET_POINTS_COLOR
+from l5kit.geometry import transform_points
+from tqdm import tqdm
+from collections import Counter
+from l5kit.data import PERCEPTION_LABELS
+from prettytable import PrettyTable
+
+from matplotlib import animation, rc
+from IPython.display import HTML
+
+rc('animation', html='jshtml')
+print("l5kit version:", l5kit.__version__)
+
+import torch
+from pathlib import Path
+
+import pytorch_pfn_extras as ppe
+from math import ceil
+from pytorch_pfn_extras.training import IgniteExtensionsManager
+from pytorch_pfn_extras.training.triggers import MinValueTrigger
+
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Subset
+import pytorch_pfn_extras.training.extensions as E
+
+
+# --- Dataset utils ---
+from typing import Callable
+
+from torch.utils.data.dataset import Dataset
+
+
+class TransformDataset(Dataset):
+    def __init__(self, dataset: Dataset, transform: Callable):
+        self.dataset = dataset
+        self.transform = transform
+
+    def __getitem__(self, index):
+        batch = self.dataset[index]
+        return self.transform(batch)
+
+    def __len__(self):
+        return len(self.dataset)
+
+# LOSS FUNCTION
+# --- Function utils ---
+# Original code from https://github.com/lyft/l5kit/blob/20ab033c01610d711c3d36e1963ecec86e8b85b6/l5kit/l5kit/evaluation/metrics.py
+import numpy as np
+
+import torch
+from torch import Tensor
+
+
+def pytorch_neg_multi_log_likelihood_batch(
+    gt: Tensor, pred: Tensor, confidences: Tensor, avails: Tensor
+) -> Tensor:
+    """
+    Compute a negative log-likelihood for the multi-modal scenario.
+    log-sum-exp trick is used here to avoid underflow and overflow, For more information about it see:
+    https://en.wikipedia.org/wiki/LogSumExp#log-sum-exp_trick_for_log-domain_calculations
+    https://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/
+    https://leimao.github.io/blog/LogSumExp/
+    Args:
+        gt (Tensor): array of shape (bs)x(time)x(2D coords)
+        pred (Tensor): array of shape (bs)x(modes)x(time)x(2D coords)
+        confidences (Tensor): array of shape (bs)x(modes) with a confidence for each mode in each sample
+        avails (Tensor): array of shape (bs)x(time) with the availability for each gt timestep
+    Returns:
+        Tensor: negative log-likelihood for this example, a single float number
+    """
+    assert len(pred.shape) == 4, f"expected 3D (MxTxC) array for pred, got {pred.shape}"
+    batch_size, num_modes, future_len, num_coords = pred.shape
+
+    assert gt.shape == (batch_size, future_len, num_coords), f"expected 2D (Time x Coords) array for gt, got {gt.shape}"
+    assert confidences.shape == (batch_size, num_modes), f"expected 1D (Modes) array for gt, got {confidences.shape}"
+    assert torch.allclose(torch.sum(confidences, dim=1), confidences.new_ones((batch_size,))), "confidences should sum to 1"
+    assert avails.shape == (batch_size, future_len), f"expected 1D (Time) array for gt, got {avails.shape}"
+    # assert all data are valid
+    assert torch.isfinite(pred).all(), "invalid value found in pred"
+    assert torch.isfinite(gt).all(), "invalid value found in gt"
+    assert torch.isfinite(confidences).all(), "invalid value found in confidences"
+    assert torch.isfinite(avails).all(), "invalid value found in avails"
+
+    # convert to (batch_size, num_modes, future_len, num_coords)
+    gt = torch.unsqueeze(gt, 1)  # add modes
+    avails = avails[:, None, :, None]  # add modes and cords
+
+    # error (batch_size, num_modes, future_len)
+    error = torch.sum(((gt - pred) * avails) ** 2, dim=-1)  # reduce coords and use availability
+
+    with np.errstate(divide="ignore"):  # when confidence is 0 log goes to -inf, but we're fine with it
+        # error (batch_size, num_modes)
+        error = torch.log(confidences) - 0.5 * torch.sum(error, dim=-1)  # reduce time
+
+    # use max aggregator on modes for numerical stability
+    # error (batch_size, num_modes)
+    max_value, _ = error.max(dim=1, keepdim=True)  # error are negative at this point, so max() gives the minimum one
+    error = -torch.log(torch.sum(torch.exp(error - max_value), dim=-1, keepdim=True)) - max_value  # reduce modes
+    # print("error", error)
+    return torch.mean(error)
+
+
+def pytorch_neg_multi_log_likelihood_single(
+    gt: Tensor, pred: Tensor, avails: Tensor
+) -> Tensor:
+    """
+
+    Args:
+        gt (Tensor): array of shape (bs)x(time)x(2D coords)
+        pred (Tensor): array of shape (bs)x(time)x(2D coords)
+        avails (Tensor): array of shape (bs)x(time) with the availability for each gt timestep
+    Returns:
+        Tensor: negative log-likelihood for this example, a single float number
+    """
+    # pred (bs)x(time)x(2D coords) --> (bs)x(mode=1)x(time)x(2D coords)
+    # create confidence (bs)x(mode=1)
+    batch_size, future_len, num_coords = pred.shape
+    confidences = pred.new_ones((batch_size, 1))
+    return pytorch_neg_multi_log_likelihood_batch(gt, pred.unsqueeze(1), confidences, avails)
+
+
+# MODEL
+# --- Model utils ---
+import torch
+from torchvision.models import resnet18
+from torch import nn
+from typing import Dict
+
+
+class LyftMultiModel(nn.Module):
+
+    def __init__(self, cfg: Dict, num_modes=3):
+        super().__init__()
+
+        # TODO: support other than resnet18?
+        backbone = resnet18(pretrained=True, progress=True)
+        self.backbone = backbone
+
+        num_history_channels = (cfg["model_params"]["history_num_frames"] + 1) * 2
+        num_in_channels = 3 + num_history_channels
+
+        self.backbone.conv1 = nn.Conv2d(
+            num_in_channels,
+            self.backbone.conv1.out_channels,
+            kernel_size=self.backbone.conv1.kernel_size,
+            stride=self.backbone.conv1.stride,
+            padding=self.backbone.conv1.padding,
+            bias=False,
+        )
+
+        # This is 512 for resnet18 and resnet34;
+        # And it is 2048 for the other resnets
+        backbone_out_features = 512
+
+        # X, Y coords for the future positions (output shape: Bx50x2)
+        self.future_len = cfg["model_params"]["future_num_frames"]
+        num_targets = 2 * self.future_len
+
+        # You can add more layers here.
+        self.head = nn.Sequential(
+            # nn.Dropout(0.2),
+            nn.Linear(in_features=backbone_out_features, out_features=4096),
+        )
+
+        self.num_preds = num_targets * num_modes
+        self.num_modes = num_modes
+
+        self.logit = nn.Linear(4096, out_features=self.num_preds + num_modes)
+
+    def forward(self, x):
+        x = self.backbone.conv1(x)
+        x = self.backbone.bn1(x)
+        x = self.backbone.relu(x)
+        x = self.backbone.maxpool(x)
+
+        x = self.backbone.layer1(x)
+        x = self.backbone.layer2(x)
+        x = self.backbone.layer3(x)
+        x = self.backbone.layer4(x)
+
+        x = self.backbone.avgpool(x)  # till here is resnet18
+        x = torch.flatten(x, 1)  # flatten to (bs x 512)
+
+        x = self.head(x)
+        x = self.logit(x)
+
+        # pred (bs)x(modes)x(time)x(2D coords)
+        # confidences (bs)x(modes)
+        bs, _ = x.shape
+        pred, confidences = torch.split(x, self.num_preds, dim=1)
+        pred = pred.view(bs, self.num_modes, self.future_len, 2)  # reshape
+        assert confidences.shape == (bs, self.num_modes)
+        confidences = torch.softmax(confidences, dim=1)
+        return pred, confidences
+
+
+class LyftMultiRegressor(nn.Module):
+    """Single mode prediction"""
+
+    def __init__(self, predictor, lossfun=pytorch_neg_multi_log_likelihood_batch):
+        super().__init__()
+        self.predictor = predictor
+        self.lossfun = lossfun
+
+    def forward(self, image, targets, target_availabilities):
+        pred, confidences = self.predictor(image)
+        loss = self.lossfun(targets, pred, confidences, target_availabilities)
+        metrics = {
+            "loss": loss.item(),
+            "nll": pytorch_neg_multi_log_likelihood_batch(targets, pred, confidences, target_availabilities).item()
+        }
+        ppe.reporting.report(metrics, self)
+        return loss, metrics
+
+
+# TRAINING WITH IGNITE
